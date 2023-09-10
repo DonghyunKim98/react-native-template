29696d51fe54642b292a30b48188f9c96d3a3dfb
DongHyunKim <akainoo123@gmail.com>
2023-09-10 22:44:20 +0900

    feat : i18n next finish

--- src/providers/i18n/i18n.provider.ts
+++ src/providers/i18n/i18n.provider.ts
@@ -1,10 +1,10 @@
 import { use } from 'i18next';
 import i18n, { initReactI18next } from 'react-i18next';
-// 현재 폰 언어 확인
 import { getLocales } from 'react-native-localize';
 
 import { englishTranslation, koreanTranslation } from './resources';
 
+// Default Namespace is translation
 const localizationResources = {
   ko: { translation: koreanTranslation },
   en: { translation: englishTranslation },
@@ -13,12 +13,12 @@ const localizationResources = {
 if (__DEV__) {
   use(initReactI18next).init({
     compatibilityJSON: 'v3',
-    lng: getLocales()[0].languageCode, // 앱에서 사용할 기본언어 설정
+    lng: getLocales()[0].languageCode,
     debug: true,
     resources: localizationResources,
     fallbackLng: 'ko',
     interpolation: {
-      escapeValue: false, // XSS 주입을 피하기 위해 설정
+      escapeValue: false,
     },
   });
 } else {
